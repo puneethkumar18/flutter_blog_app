4fda23b60c525bc1766a88e6ef0155417f8735dc
PUNEETH KUMAR <puneethkumarg96@gmail.com>
2024-11-26 19:08:34 +0530

    created the user table in supabase

---
+++ lib/init_dependencies.dart
@@ -0,0 +1,44 @@
+import 'package:blog_app/core/screats/app_screats.dart';
+import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:blog_app/features/auth/data/repositories/auth_repositary_impl.dart';
+import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
+import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+final serviceLocator = GetIt.asNewInstance();
+
+Future<void> initDependencies() async {
+  _initAuth();
+  final supaBase = await Supabase.initialize(
+    anonKey: AppScreats.supabaseAnnonKey,
+    url: AppScreats.supabaseUrl,
+  );
+  serviceLocator.registerLazySingleton(() => supaBase.client);
+
+  serviceLocator.registerFactory<AuthRemoteDataSource>(
+    () => AuthRemoteDataSourceImpl(
+      serviceLocator(),
+    ),
+  );
+
+  serviceLocator.registerFactory<AuthRepository>(
+    () => AuthRepositaryImpl(
+      serviceLocator(),
+    ),
+  );
+  serviceLocator.registerFactory(
+    () => UserSignUp(
+      serviceLocator(),
+    ),
+  );
+
+  serviceLocator.registerLazySingleton(
+    () => AuthBloc(
+      userSignUp: serviceLocator(),
+    ),
+  );
+}
+
+void _initAuth() {}
--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,16 @@
-import 'package:blog_app/core/screats/app_screats.dart';
 import 'package:blog_app/core/theme/theme.dart';
-import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
-import 'package:blog_app/features/auth/data/repositories/auth_repositary_impl.dart';
-import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:blog_app/features/auth/presentation/pages/login_page.dart';
+import 'package:blog_app/init_dependencies.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final supaBase = await Supabase.initialize(
-      anonKey: AppScreats.supabaseAnnonKey, url: AppScreats.supabaseUrl);
+  initDependencies();
   runApp(MultiBlocProvider(providers: [
     BlocProvider(
-      create: (_) => AuthBloc(
-        userSignUp: UserSignUp(
-          AuthRepositaryImpl(
-            AuthRemoteDataSourceImpl(supaBase.client),
-          ),
-        ),
-      ),
+      create: (_) => serviceLocator<AuthBloc>(),
     ),
   ], child: const MyApp()));
 }
